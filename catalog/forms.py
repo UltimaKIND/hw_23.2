e2b044331f3fcb54020e37edb735eb1e686576b6
UltimaKIND <minion9@mail.ru>
2024-09-08 10:53:42 +0500

    формы стилизованы через класс-миксинксин

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,13 +1,24 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, BooleanField
 
 from catalog.models import Product, Release
 
-class ProductForm(ModelForm):
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance (field, BooleanField):
+                field.widget.attrs['class'] = "form-check-input"
+            else:
+                field.widget.attrs['class'] = "form-control"
+                
+            
+
+class ProductForm(StyleFormMixin, ModelForm):
     class Meta:
         model = Product
         exclude = ('created_at', 'update_at')
 
-class ReleaseForm(ModelForm):
+class ReleaseForm(StyleFormMixin, ModelForm):
     class Meta:
         model = Release
         fields = '__all__'
