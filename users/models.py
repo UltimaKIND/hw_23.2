c9f1ed5cb6f5239b57494a13968dec26c4e29aa6
UltimaKIND <minion9@mail.ru>
2024-09-14 13:13:58 +0500

    создана модель пользователя по заданным условиям

---
+++ users/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import AbstractBaseUser
+from catalog.models import NULLABLE
+from django.db import models
+
+class User(AbstractBaseUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='почта')
+    phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+    country = models.CharField(max_length=50, verbose_name='страна', **NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
