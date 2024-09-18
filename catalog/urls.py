4a2a816455c4b5a249d009039369d3ff0c8c35b3
UltimaKIND <minion9@mail.ru>
2024-09-18 22:27:35 +0500

    добавлена кеширование для контроллера ProductDetailview

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
 from catalog.views import ContactsPageView, ProductCreateView, ProductListView, ProductDetailView, ProductUpdateView, ProductDeleteView
+from django.views.decorators.cache import cache_page
 
 app_name = CatalogConfig.name
 
@@ -8,7 +9,8 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('create', ProductCreateView.as_view(), name='create'),
     path('', ProductListView.as_view(), name='product_list'),
-    path('product/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
+    path('product/<int:pk>', cache_page(60)
+         (ProductDetailView.as_view()), name='product_detail'),
     path('edit/<int:pk>', ProductUpdateView.as_view(), name='edit'),
     path('delete/<int:pk>', ProductDeleteView.as_view(), name='delete'),
     path('contacts/', ContactsPageView.as_view(), name='contacts'),
